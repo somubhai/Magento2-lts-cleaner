7ebd0deae600aa70a58f1481dd7e38f623d95990
somubhai <somnathmondal123@gmail.com>
2017-04-25 18:53:55 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lozingle
+ * Date: 25/04/2017
+ * Time: 12:03 PM
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lts_CacheReindex',
+    __DIR__
+);
